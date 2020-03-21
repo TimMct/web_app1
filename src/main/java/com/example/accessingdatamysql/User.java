873e43cb8fdd970dc8a1bfaf750a9ff966f5d3bc
Timotei Molcut <timoteim98@gmail.com>
2020-03-21 19:07:48 +0200

    modify user controller

--- src/main/java/com/example/accessingdatamysql/User.java
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -5,14 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
-
     private String name;
-
     private String email;
 
     public Integer getId() {
---
+++ src/main/java/com/example/accessingdatamysql/UserController.java
@@ -0,0 +1,83 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    //create
+    @PostMapping(path="/add")
+    public  String addNewUser ( String name,  String email) {
+        User n = new User();
+
+        if(name.equals(null) || name.equals(""))
+            return "Must enter a valid name.";
+        else
+            n.setName(name);
+        if(email.equals(null) || email.equals("") || !email.contains("@") || !email.contains("."))
+            return "Must enter an valid email.";
+        else
+            n.setEmail(email);
+        userRepository.save(n);
+        return "User created.";
+    }
+
+    //read
+    @GetMapping(path="/all")
+    public  Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    //update
+    @PostMapping(path="/updateName")
+    public String updateUserName(String oldName, String newName){
+        Iterable<User> allUsers = userRepository.findAll();
+        if(newName.equals(null) || newName.equals(""))
+            return "Must enter a new valid name.";
+        for(User u : allUsers){
+            if(u.getName().equals(oldName)){
+                u.setName(newName);
+                userRepository.save(u);
+                return "Name updated.";
+            }
+        }
+        return "No user has the name:"+oldName+".";
+    }
+
+    @PostMapping(path="/updateEmail")
+    public String updateUserEmail(String oldEmail, String newEmail){
+        Iterable<User> allUsers = userRepository.findAll();
+        if(newEmail.equals(null) || newEmail.equals("") || !newEmail.contains("@") || !newEmail.contains("."))
+            return "Must enter a new valid email.";
+        for(User u : allUsers){
+            if(u.getEmail().equals(oldEmail)){
+                u.setEmail(newEmail);
+                userRepository.save(u);
+                return "Email updated.";
+            }
+        }
+        return "No user has the email:"+oldEmail+".";
+    }
+
+    //delete
+    @PostMapping(path="/delete")
+    public String delete(Integer id){
+        if(userRepository.existsById(id)){
+            userRepository.deleteById(id);
+            return "User deleted.";
+        }
+        else
+            return "Non-existent user.";
+    }
+
+    @GetMapping(path="/deleteAll")
+    public String deleteAll(){
+        userRepository.deleteAll();
+        return "Users deleted.";
+    }
+}
